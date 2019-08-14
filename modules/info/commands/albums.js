9de0a4b2a28263b90bf0c19c1f04a57781e9607a
esfox <osairis100@gmail.com>
2019-08-14 21:53:20 +0800

    Updated "albums" command

--- modules/info/commands/albums.js
+++ modules/info/commands/albums.js
@@ -2,6 +2,28 @@ const { Command } = require('discord-utils');
 const PagedEmbed = require('utils/PagedEmbed');
 const { albums: music } = require('data/music.json');
 
+const list = albums => albums
+  .map((album, i) => `${i + 1}. ${album.title}`)
+  .join('\n');
+
+/** @type {Array} */
+const albums = Object.values(music);
+const { korean, japanese } = albums.reduce((albums, album) => 
+{
+  albums[!album.isJapanese? 'korean' : 'japanese'].push(album);
+  return albums;
+},
+{
+  korean: [],
+  japanese: []
+});
+
+const pages = 
+[
+  `**Korean**\n\n${list(korean)}`,
+  `**Japanese**\n\n${list(japanese)}`
+];
+
 module.exports = class extends Command
 {
   constructor()
@@ -16,28 +38,6 @@ module.exports = class extends Command
 /** @param {import('discord-utils').Context} context*/
 function action(context)
 {
-  /** @type {Array} */
-  const albums = Object.values(music);
-  const { korean, japanese } = albums.reduce((albums, album) => 
-  {
-    albums[!album.isJapanese? 'korean' : 'japanese'].push(album);
-    return albums;
-  },
-  {
-    korean: [],
-    japanese: []
-  });
-
-  const pages = 
-  [
-    `**Korean**\n\n${list(korean)}`,
-    `**Japanese**\n\n${list(japanese)}`
-  ];
-
   new PagedEmbed().send(context, '💿  TWICE Albums', pages,
     { thumbnail: 'https://i.imgur.com/hhfIJUF.gif' });
 }
-
-const list = albums => albums
-  .map((album, i) => `${i + 1}. ${album.title}`)
-  .join('\n');
