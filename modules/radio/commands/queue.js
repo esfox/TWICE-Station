3bfceb9cb01d7791fe94d01a09bef66aa8019f5a
esfox <osairis100@gmail.com>
2019-08-31 13:00:59 +0800

    Fixed "queue" command

--- modules/radio/commands/queue.js
+++ modules/radio/commands/queue.js
@@ -21,7 +21,7 @@ async function action(context)
     return context.send('No songs are queued.');
 
   tracks = tracks.slice(1).reduce((list, { title }, i) =>
-    list + `${i + 1}. ${title}\n`);
+    list + `${i + 1}. ${title}\n`, '');
 
   const embed = context.embed('▶  Play Queue');
   if(queue.nowPlaying)
