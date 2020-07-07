509b07f4882504fac672daf437519e727384f9e1
esfox <osairis100@gmail.com>
2020-07-07 22:42:56 +0800

    Updated rewarder bug (sometimes not working)

--- bot/modules/games/rewarder.js
+++ bot/modules/games/rewarder.js
@@ -6,7 +6,4 @@ const { Coins } = require('api/models')
  * @param {string} user Discord of the user to reward.
  * @param {number} amount Amount of twicecoins to reward.
  */
-exports.giveReward = async (user, amount) =>
-{
-  await Coins.addToUser(user, amount);
-}
+exports.giveReward = async (user, amount) => await Coins.addToUser(user, amount);
